fe904a6b20b7e68d02b93680dd9a3b1a3ee6ff0d
sauves <leonardsauve@gmail.com>
2022-07-09 15:31:20 -0400

    added stuff

--- FE_inference.jl
+++ FE_inference.jl
@@ -28,3 +28,33 @@ wd = 1e-3
 patient_emb_size = 2
 gene_emb_size = 50 
 
+struct Network2{A, B}
+    emb_layer_1::Flux.Embedding{A}
+    emb_layer_2::Flux.Embedding{A}
+    model::Flux.Chain{B}
+end 
+
+function get_encoder(net::Network2) 
+    layer = net.model[1]
+    return layer
+end
+
+function nn(emb_size_1::Int, emb_size_2::Int, f1_size::Int, f2_size::Int) 
+    a = emb_size_1 + emb_size_2
+    b, c = 50, 10
+    emb_layer_1 = gpu(Flux.Embedding(f1_size, emb_size_1))
+    emb_layer_2 = gpu(Flux.Embedding(f2_size, emb_size_2))
+    model=Chain(
+        Flux.Parallel(vcat, emb_layer_1, emb_layer_2),
+        Dense(a, b, relu), 
+        Dense(b, c, relu),
+        Dense(c, 1, identity),
+        vec
+    )
+    return Network2(emb_layer_1, emb_layer_2, model)
+end
+
+function nn(net::Network2, new_f1_size::Int)
+    new_emb = Flux.Embedding(new_f1_size, size(net.emb_layer_1.weight)[1] ) |> gpu
+    return Network2(new_emb, net.emb_layer_2)
+end
--- init.jl
+++ init.jl
@@ -3,8 +3,14 @@ if isfile("Project.toml") && isfile("Manifest.toml")
     Pkg.activate(".")
 end
 @time using RedefStructs 
+@time using Dates
 @time using Statistics
 @time using CUDA
 @time using DataFrames
 @time using CSV
 @time using Flux
+
+basepath = "/u/sauves/leonard_leucegene_factorized_embeddings/"
+outpath  = "./RES/EMBEDDINGS" # our output directory
+outdir = "$(outpath)/embeddings_$(now())"
+mkdir(outdir)
